d18f4131923890302dd79d56ec62bb8c84b549e3
ambyra <ambyra@gmail.com>
2023-04-23 11:37:23 -0600

    appointment state control

--- src/sample/controller/appointmentController.java
+++ src/sample/controller/appointmentController.java
@@ -1,17 +1,13 @@
 package sample.controller;
 
-import sample.dao.AppointmentDAO;
-
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import sample.dao.AutoTableView;
-import sample.model.Appointment;
 
 import java.net.URL;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class appointmentController implements Initializable {
@@ -30,24 +26,79 @@ public class appointmentController implements Initializable {
     @FXML private TextField TextFieldTitle;
     @FXML private TextField TextFieldType;
 
+    @FXML private Button ButtonAdd;
+    @FXML private Button ButtonEdit;
+    @FXML private Button ButtonSave;
+    @FXML private Button ButtonCancel;
+    @FXML private Button ButtonDelete;
+
+
     private ObservableList<ObservableList> data;
+
+    void selectState(){
+        TableViewAppointments.setDisable(false);
+        clearBoxes();
+        disableButtons();
+        ButtonAdd.setDisable(false);
+        ButtonEdit.setDisable(false);
+        ButtonDelete.setDisable(false);
+    }
+    void addState(){
+        TableViewAppointments.setDisable(true);
+        disableButtons();
+        ButtonSave.setDisable(false);
+        ButtonCancel.setDisable(false);
+    }
+    void editState(){
+        TableViewAppointments.setDisable(true);
+        disableButtons();
+        ButtonSave.setDisable(false);
+        ButtonCancel.setDisable(false);
+    }
+    void clearBoxes(){
+        TextFieldAppointmentID.clear();
+        TextFieldDescription.clear();
+        TextFieldEndTime.clear();
+        TextFieldLocation.clear();
+        TextFieldStartTime.clear();
+        TextFieldTitle.clear();
+        TextFieldType.clear();
+    }
+
+    void disableButtons(){
+        ButtonAdd.setDisable(true);
+        ButtonEdit.setDisable(true);
+        ButtonSave.setDisable(true);
+        ButtonCancel.setDisable(true);
+        ButtonDelete.setDisable(true);
+    }
 
     @FXML
     void ClickAdd (ActionEvent event){
+        addState();
     }
 
     @FXML
     void ClickDelete (ActionEvent event){
+        selectState();
     }
 
     @FXML
-    void ClickUpdate (ActionEvent event){
+    void ClickCancel (ActionEvent event){
+        selectState();
     }
 
     @FXML
-    void ClickLoad(ActionEvent event) {
+    void ClickSave (ActionEvent event){
+        selectState();
+    }
+
+    @FXML
+    void ClickEdit(ActionEvent event) {
+        editState();
         ObservableList<Object> selectedItem= TableViewAppointments.getSelectionModel().getSelectedItem();
         System.out.println(selectedItem);
+        if (selectedItem == null){return;}
 
         TextFieldAppointmentID.setText((String) selectedItem.get(0));
         TextFieldTitle.setText((String) selectedItem.get(1));
@@ -59,11 +110,15 @@ public class appointmentController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        selectState();
+
         AutoTableView autoTableAppointments = new AutoTableView(TableViewAppointments);
         String query =
             "select appointment_id, title, description, location, contact_id, type, start, end, customer_id, user_id" +
             " from client_schedule.appointments";
         autoTableAppointments.loadTableFromDB(query);
+
+        TableViewAppointments.getSelectionModel().selectFirst();
 
     }
 }
